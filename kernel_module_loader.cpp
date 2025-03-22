db6a4047f6874518877188e078b9dd1ee485db13
ymdzq <ymdzq@users.noreply.github.com>
2025-03-22 12:15:52 +0800

    提前加载预置goodix_core模块
    调整TW_XIAOMI_TOUCH_RMMOD_GOODIX_CORE变量的效果，用于解决相关机型（k60、note12turbo）澎湃os的goodix_core问题
    在这个阶段加载模块可以减少一大堆相关错误的日志刷屏
    https://github.com/ymdzq/SK_Recovery/commit/9c687f107e25c94cc0b5afc6a71771d1aa63b773

--- kernel_module_loader.cpp
+++ kernel_module_loader.cpp
@@ -63,7 +63,7 @@ bool KernelModuleLoader::Load_Vendor_Modules() {
 #ifdef TW_XIAOMI_TOUCH_RMMOD_GOODIX_CORE
 	{
 		std::string result;
-		TWFunc::Exec_Cmd("rmmod goodix_core > /dev/null 2>&1", result, false);
+		TWFunc::Exec_Cmd("rmmod goodix_core && insmod /vendor/lib/modules/1.1/goodix_core.ko > /dev/null 2>&1", result, false);
 	}
 #endif
 
